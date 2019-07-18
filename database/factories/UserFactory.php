203eaa40314c3a76902d1ea64343159270a1a474
androidborn <tweaksbuzz@gmail.com>
2019-07-18 16:49:26 +0530

    hasManyThough

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,8 @@
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\posts;
+use App\country;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +25,25 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'country_id' => function()
+        {
+            return factory('App\country')->create()->id;
+        }
+    ];
+});
+
+$factory->define(posts::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'user_id' => function()
+        {
+            return factory('App\User')->create()->id;
+        }
+    ];
+});
+
+$factory->define(country::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
     ];
 });
